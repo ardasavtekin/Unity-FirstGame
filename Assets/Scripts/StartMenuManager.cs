5d2c65c133badec02078358f667a86324567f4c9
Hakan Mutlu <hakannurimutlu@gmail.com>
2021-08-19 12:11:05 +0300

    awfsfa
    afascasa

---
+++ Assets/Scripts/StartMenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuManager : MonoBehaviour
+{
+    public string GameScreen;
+    public string Settings;
+    public void GameScene()
+    {
+        SceneManager.LoadScene(GameScreen);
+        
+    }
+    public void SettingsScene()
+    {
+        SceneManager.LoadScene(Settings);
+    }
+}
