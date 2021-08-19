d3abef32a5247059103a3fdc0bcb8bf5cd365a2b
Arda Berke Savtekin <savtekinarda@gmail.com>
2021-08-19 13:45:10 +0300

    UI
    Sahne geçişleri ayarlandı

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public GameObject PauseMenuCanvas;
+    public GameObject PauseMenuButton;
+    public GameObject ResumeButton;
+
+    public void Start()
+    {
+        PauseMenuCanvas.SetActive(false);
+
+    }
+
+    
+    public void changeScene(int sceneId)
+    {
+        SceneManager.LoadScene(sceneId);
+    }
+
+    public void PauseGame()
+    {
+        PauseMenuCanvas.SetActive(true);
+        PauseMenuButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenuCanvas.SetActive(false);
+        PauseMenuButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+}
